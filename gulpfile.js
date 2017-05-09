2a84035e2af47d1fee383615b77161bcf53d4c5a
Stefaan Vandenborne <stefanvandenborne@gmail.com>
2017-05-09 18:23:35 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+
+/**
+ * Created by stefan on 09.05.17.
+ */
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var minifyCSS = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var clean = require('gulp-clean');
+var sourcemaps = require('gulp-sourcemaps');
+//var browserSync = require('browser-sync').create();
+
+// //////////////////////////////////////////////
+// Sass to css
+// /////////////////////////////////////////////
+gulp.task('sass', function () {
+    return gulp.src('sass/styles.sass')
+        .pipe(sourcemaps.init())
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('minifyCSS', function () {
+    return gulp.src('css/styles.css')
+        .pipe(minifyCSS({keepBreaks: true}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('css'))
+});
+
+// ///////////////////////////////////////////////////
+// build folder
+// //////////////////////////////////////////////////
+
+var filesToMove = [
+    './bower_components/**/*.*',
+    './css/styles.min.css',
+    './fonts/**/*.*',
+    './images/**/*.*',
+    './js/**/*.*',
+    './templates/**/*.*',
+    './jpkot.info.yml',
+    './jpkot.libraries.yml',
+    './jpkot.theme',
+    './readme.md',
+    './screenshot.png'
+];
+
+gulp.task('clean', function(){
+    return gulp.src(['build/*'], {read:false})
+        .pipe(clean());
+});
+
+
+gulp.task('move',['clean'], function(){
+    // the base option sets the relative root for the set of files,
+    // preserving the folder structure
+    gulp.src(filesToMove, { base: './' })
+        .pipe(gulp.dest('build'));
+});
+
+// ///////////////////////////////////////////////////
+// Watch Task
+// ///////////////////////////////////////////////////
+gulp.task('watch', function(){
+    gulp.watch('sass/**/*.{scss,sass}', ['sass']);
+});
+
+// ///////////////////////////////////////////////////
+// Default Task
+// ///////////////////////////////////////////////////
+gulp.task('default' , ['sass', 'minifyCSS', 'watch']);
---
+++ templates/php/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stefan
+ * Date: 09.05.17
+ * Time: 17:48
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Spitfireclub Vlaanderen</title>
+</head>
+<body>
+
+</body>
+</html>
