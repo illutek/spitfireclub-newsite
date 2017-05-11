8caf6412b02f3c978abfeaf41fda6a61e85813b1
Vandenborne Stefan <stefan@illutek.be>
2017-05-11 13:43:31 +0200

    start opbouw homepage

--- js/backstretch.js
+++ js/backstretch.js
@@ -1,6 +1,14 @@
 /**
  * Created by Stefan on 10/05/2017.
  */
-$(document).ready(function () {
-
-});
+(function ($) {
+    $(document).ready(function () {
+        $.backstretch([
+            "../../images/gele-spit.jpg",
+            "../../images/rode-spit.jpg"
+        ],{
+            fade: 750,
+            duration: 4000
+        });
+    });
+})(jQuery);
