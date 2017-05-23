0ff735853d2a253c0c4d0ef467763d9856704871
stefan <stefan@illutek.be>
2017-05-23 22:56:31 +0200

    gulp css-compress ipv minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
-
 /**
  * Created by stefan on 09.05.17.
  */
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
-var minifyCSS = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var clean = require('gulp-clean');
 var sourcemaps = require('gulp-sourcemaps');
+var imagemin = require('gulp-imagemin');
 //var browserSync = require('browser-sync').create();
 
 // //////////////////////////////////////////////
@@ -18,17 +17,11 @@ gulp.task('sass', function () {
     return gulp.src('sass/styles.sass')
         .pipe(sourcemaps.init())
         .pipe(plumber())
-        .pipe(sass())
+        .pipe(sass({outputStyle: 'compressed'}).on('error',sass.logError))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('css'));
 });
 
-gulp.task('minifyCSS', function () {
-    return gulp.src('css/styles.css')
-        .pipe(minifyCSS({keepBreaks: true}))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('css'))
-});
 
 // ///////////////////////////////////////////////////
 // build folder
@@ -36,7 +29,7 @@ gulp.task('minifyCSS', function () {
 
 var filesToMove = [
     './bower_components/**/*.*',
-    './css/styles.min.css',
+    './css/styles.css',
     './fonts/**/*.*',
     './images/**/*.*',
     './js/**/*.*',
@@ -48,27 +41,37 @@ var filesToMove = [
     './screenshot.png'
 ];
 
-gulp.task('clean', function(){
-    return gulp.src(['build/*'], {read:false})
+gulp.task('clean', function () {
+    return gulp.src(['dist/*'], {read: false})
         .pipe(clean());
 });
 
 
-gulp.task('move',['clean'], function(){
+gulp.task('dist', ['clean'], function () {
     // the base option sets the relative root for the set of files,
     // preserving the folder structure
-    gulp.src(filesToMove, { base: './' })
+    gulp.src(filesToMove, {base: './'})
         .pipe(gulp.dest('dist'));
+});
+
+// /////////////////////////////////////////////////
+// MINIFY IMAGES
+// ////////////////////////////////////////////////
+
+gulp.task('compress-images', function(){
+    return gulp.src('images/*')
+        .pipe(imagemin({ progressive: true }))
+        .pipe(gulp.dest('dist/images/'));
 });
 
 // ///////////////////////////////////////////////////
 // Watch Task
 // ///////////////////////////////////////////////////
-gulp.task('watch', function(){
+gulp.task('watch', function () {
     gulp.watch('sass/**/*.{scss,sass}', ['sass']);
 });
 
 // ///////////////////////////////////////////////////
 // Default Task
 // ///////////////////////////////////////////////////
-gulp.task('default' , ['sass', 'minifyCSS', 'watch']);
+gulp.task('default', ['sass', 'watch']);
