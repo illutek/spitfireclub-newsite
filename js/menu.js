1d4ff1a597baddb9fde8f9cdd377726aacce8353
stefan <stefan@illutek.be>
2017-05-28 14:57:58 +0200

    klikbaar gebied menu-hamburger vergroot

--- js/menu.js
+++ js/menu.js
@@ -4,16 +4,13 @@
 (function ($) {
     $(document).ready(function () {
         $('.menu-left-wrapper').removeClass('menu-left__visible');
-        $('.hamburger-img').removeClass('menu-wrap__visible');
 
-        $('.hamburger-img').click(function () {
+        $('.btn-hamburger_menu').click(function () {
             $('.menu-left-wrapper').addClass('menu-left__visible');
-            $('.hamburger-img').addClass('menu-wrap__visible');
         });
 
         $('.close-btn').click(function () {
             $('.menu-left-wrapper').removeClass('menu-left__visible');
-            $('.hamburger-img').removeClass('menu-wrap__visible');
         });
     });
 })(jQuery);
