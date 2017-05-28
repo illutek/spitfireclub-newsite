ff1f1b43abb5ccaf3f9f6224271a37dbbf0a2b19
Vandenborne Stefan <stefan@illutek.be>
2017-05-28 14:14:40 +0200

    extra image to home background

--- js/backstretch_drupal.js
+++ js/backstretch_drupal.js
@@ -8,7 +8,8 @@
             base + "images/frontpage-images/gele-spit.jpg",
             base + "images/frontpage-images/groene-MKI-spit.jpg",
             base + "images/frontpage-images/rode-spit.jpg",
-            base + "images/frontpage-images/groene-spit.jpg"
+            base + "images/frontpage-images/groene-spit.jpg",
+            base + "images/frontpage-images/groep.jpg"
         ],{
             fade: 750,
             duration: 4000
